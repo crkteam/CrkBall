fd5a590b70e30e349a997c1c0184d704a3191dc4
crkteam <39040121+crkteam@users.noreply.github.com>
2018-12-07 23:04:40 +0800

    crk
    init

---
+++ CrkBall/Assets/Script/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private Rigidbody2D rigidbody2D;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+		rigidbody2D.velocity = new Vector2(5,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+}
