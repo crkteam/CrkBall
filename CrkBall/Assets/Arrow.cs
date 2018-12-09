2bd86748ed7591ed20883bc434f00304c91c8ec1
crkGG <abab05056@gmail.com>
2018-12-09 21:57:16 +0800

    crkgg
    crkGG

---
+++ CrkBall/Assets/Arrow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Arrow : MonoBehaviour,IPointerDownHandler
+{
+	public int kind;
+
+	public KindController kc;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public  void OnPointerDown(PointerEventData eventData )
+	{
+		kc.increase(kind);
+		
+		kc.MovePoint(kind);
+	}
+}
