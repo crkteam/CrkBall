ffa413020f0a751b0510a094c1fdfa5e029c65df
crkGG <abab05056@gmail.com>
2018-12-12 01:07:52 +0800

    crkGG

---
+++ CrkBall/Assets/NextScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour,IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public  void OnPointerDown(PointerEventData eventData)
+	{
+        		SceneManager.LoadScene("Main");
+	}
+	
+}
---
+++ CrkBall/Assets/Resources/BallImage/CreateImage.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateImage : MonoBehaviour
+{
+    private int[] json;
+    private int point = 0;
+    private List<GameObject> ballimage;
+    public GameObject BigBallimage;
+    private void Awake()
+    {
+        ballimage = new List<GameObject>();
+        json = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  
+        GameObject image = Resources.Load<GameObject>("BallImage/Item");
+
+
+        foreach (var value in json)
+        {
+            GameObject buffer = Instantiate(image);
+            buffer.GetComponent<Image>().sprite = Resources.Load<Sprite>("BallImage/" + value);
+            buffer.transform.position = gameObject.transform.position;
+            buffer.transform.position += new Vector3((value - 1) * 250, 0, 0);
+            buffer.transform.localScale = new Vector3(1, 1, 1);
+            buffer.transform.SetParent(gameObject.transform);
+            ballimage.Add(buffer);
+        
+        }
+    }
+
+    void initScale()
+    {
+        foreach (var Scale in ballimage)
+        {
+            Scale.transform.localScale=new Vector3(0f,0f,0f);
+        }
+        ballimage[point].transform.localScale=new Vector3(1f,1f,1f);
+        if (point - 1 >= 0)
+        ballimage[point-1].transform.localScale=new Vector3(0.5f,.5f,.5f);
+        if(point+1<=ballimage.Count-1)
+        ballimage[point+1].transform.localScale=new Vector3(.5f,.5f,.5f);
+        BigBallimage.GetComponent<Image>().sprite = ballimage[point].GetComponent<Image>().sprite;
+       
+    }
+    public void MovePoint(float move)
+    {
+        if (move > 0)
+        {
+            StartCoroutine(ScaleToL());
+        }
+        else
+        {
+            StartCoroutine(ScaleToR());
+        } 
+        StartCoroutine(Move(move)); //移動250
+    }
+
+    IEnumerator ScaleToL()
+    {
+        float ScalePoint=0;
+        while (true)
+        {
+     
+            ScalePoint += 0.04f;
+           
+           ballimage[point].transform.localScale+=new Vector3(.04f,.04f,.04f);
+            if (point - 1 >= 0)
+            {
+                ballimage[point-1].transform.localScale -= new Vector3(.04f, .04f, .04f);
+            }
+            if (point - 2 >= 0)    
+            {
+                ballimage[point-2].transform.localScale -= new Vector3(.04f, .04f, .04f);
+            }
+            if(point+1<=ballimage.Count-1)
+            ballimage[point+1].transform.localScale+=new Vector3(.04f,.04f,.04f);
+            yield return  new  WaitForSeconds(.01f);
+            if (ScalePoint>=.5f)
+            {
+               break; 
+            }
+        }
+        initScale();
+        
+    }
+    IEnumerator ScaleToR()
+    {
+        float ScalePoint=0;
+        while (true)
+        {
+            ScalePoint += 0.04f;
+            ballimage[point].transform.localScale+=new Vector3(.04f,.04f,.04f);
+            if (point - 1 >= 0)
+            {
+                ballimage[point-1].transform.localScale += new Vector3(.04f, .04f, .04f);
+            }
+            if(point+1<=ballimage.Count)
+                ballimage[point+1].transform.localScale-=new Vector3(.04f,.04f,.04f);
+            if(point+2<=ballimage.Count-1)
+                ballimage[point+2].transform.localScale-=new Vector3(.04f,.04f,.04f);
+            yield return  new  WaitForSeconds(.01f);
+            if (ScalePoint>=.5f)
+            {
+                break; 
+            }
+        }
+        initScale();
+    }
+    IEnumerator  Move(float move)
+    {
+        int countp=0;
+        while (true)
+        {
+            countp += 50;
+            foreach (var moveG in ballimage)
+            {
+              
+                moveG.transform.position-=new Vector3(move*50,0f,0f);
+				
+            }
+
+            if (countp >= 250)
+            {
+               
+                break;
+            }
+            yield return new WaitForSeconds(.01f);
+			
+
+        }
+      
+    }
+
+    public void check(GameObject Arrow,GameObject Arrowopposite)
+    {
+        if (point == 0 || point == ballimage.Count-1)
+        {
+            Arrow.SetActive(false);;
+        }
+        else
+        {
+			
+            Arrowopposite.SetActive(true);
+        }
+    }
+    public void increase(int x)
+    {
+        point += x;
+		
+    }
+    // Use this for initialization
+    void Start()
+    {
+        initScale();
+    }
+
+    public int Getkind()
+    {
+        return point;
+    }
+
+    public int GetBallimageCount()
+    {
+        return ballimage.Count;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
