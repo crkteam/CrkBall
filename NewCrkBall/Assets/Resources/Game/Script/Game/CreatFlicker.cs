619917bdcf60b69b4a3064597b4e863268a2a5ff
crkGG <abab05056@gmail.com>
2019-01-20 18:59:40 +0800

    crkGG

---
+++ NewCrkBall/Assets/Resources/Game/Script/Game/CreatFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatFlicker : MonoBehaviour
+{
+
+	public GameObject flicker;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CreateObject",1f,.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateObject()
+	{
+		Debug.Log(gameObject.transform.position.y);
+		Vector3 InstantiatePosition=new Vector3(Random.Range(2.5f,-2.5f),gameObject.transform.position.y,0);
+		GameObject prefab = Instantiate(flicker,gameObject.transform);
+		prefab.transform.position = InstantiatePosition;
+	}
+}
---
+++ NewCrkBall/Assets/Resources/Game/Script/Game/flicker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flicker : MonoBehaviour
+{
+    private AudioSource audio;
+    public GameObject GameControll;
+    private float[] samples = new float[8192];
+
+    private float temp;
+
+// Use this for initialization
+    void Start()
+    {
+        GameControll = GameObject.Find("Game");
+        Destroy(gameObject, 4f);
+        audio = GameControll.GetComponent<AudioSource>();
+        // Use this for initialization
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audio.GetSpectrumData(samples, 0, FFTWindow.BlackmanHarris);
+        for (int i = 0; i < samples.Length; i++)
+        {
+            temp += samples[i] * .3f;
+        }
+
+        gameObject.transform.localScale = new Vector3(temp, temp, gameObject.transform.localScale.z);
+
+        temp = 0;
+        Color Color = gameObject.GetComponent<SpriteRenderer>().color;
+        Color.a -= 0.005f;
+        gameObject.GetComponent<SpriteRenderer>().color = Color;
+        gameObject.transform.position += Vector3.up * Time.deltaTime*1.5f;
+    }
+}   
