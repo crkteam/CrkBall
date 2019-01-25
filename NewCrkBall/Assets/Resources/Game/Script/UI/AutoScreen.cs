3439de94a3c21509915e6ceecb084c03e0936b92
crkGG <abab05056@gmail.com>
2019-01-25 13:55:46 +0800

    crkGG

--- NewCrkBall/Assets/Resources/Game/Script/UI/AutoScreen.cs
+++ NewCrkBall/Assets/Resources/Game/Script/UI/AutoScreen.cs
@@ -8,17 +8,14 @@ public class AutoScreen : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-		float factor =1920f/(GetComponent<Camera>().orthographicSize*2); //高比
-		float neworthographicSize = 1080 / factor;
-		if (Screen.height / Screen.width >= 2)
-		{
-			Camera.GetComponent<Camera>().orthographicSize = neworthographicSize;
-			GameObject AutoBackgroundUp = Instantiate(Background);
-			GameObject AutoBackgroundDown = Instantiate(Background);
-			AutoBackgroundUp.transform.position = new Vector2(0, 5.5f);
-			AutoBackgroundDown.transform.position = new Vector2(0, -5.5f);
-		}
+		float newOrthographicSize = ((float) Screen.height / (float) Screen.width) * 1080f / 1920f * 4.96f;
+		Camera.GetComponent<Camera>().orthographicSize = newOrthographicSize;
+		GameObject AutoBackgroundUp = Instantiate(Background);
+		GameObject AutoBackgroundDown = Instantiate(Background);
+		AutoBackgroundUp.transform.position = new Vector2(0, 5.5f);
+		AutoBackgroundDown.transform.position = new Vector2(0, -5.5f);
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
